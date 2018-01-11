a1d724f7cc1fb1632f9ede62c357c34ecc866898
Team3482 <Team3482@10.6.81.123>
2018-01-10 17:45:19 -0800

    Coded TurnAuto, turning for autonomous

---
+++ PowerUp2018/src/org/usfirst/frc/team3482/robot/command/TurnAuto.java
@@ -0,0 +1,37 @@
+package org.usfirst.frc.team3482.robot.command;
+
+import org.usfirst.frc.team3482.robot.RobotMap;
+
+import edu.wpi.first.wpilibj.command.Command;
+
+public class TurnAuto extends Command {
+	
+	double angle;
+	
+	public TurnAuto(int timedOut, double angle){
+		super(timedOut);
+		this.angle= angle;
+	}
+	
+	protected void initialize()
+	{
+		RobotMap.drive.arcadeDrive(0, angle);
+		//speed and angle for rotation
+	}
+
+	protected void execute()
+	{
+		
+	}
+	
+	protected void end()
+	{
+		//stop all rotation.
+		RobotMap.drive.arcadeDrive(0, 0);
+	}
+	
+	protected boolean isFinished()
+	{
+		return isTimedOut();
+	}
+}
